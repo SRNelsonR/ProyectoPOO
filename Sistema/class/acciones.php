ef984d4ae653afe541836ec30b749d918bdc50c1
Julissa-Castro <julissarafaelacastrosalgado@yahoo.es>
2016-11-10 16:19:24 -0600

    acciones del Administrador

---
+++ Sistema/class/acciones.php
@@ -0,0 +1,24 @@
+<?php
+	switch ($_GET["accion"]) {
+		
+		case '1':
+			# code...
+			break;
+		case '2':
+			# code...
+			break;
+		case '3':
+			# code...
+			break;
+		case '4':
+			# code...
+			break;
+		case '5':
+			# code...
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+?>
