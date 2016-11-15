70f8c4dcd788857844cc168d61a597dbe0893fed
Oscar Nelson <oscarnelson18@gmail.com>
2016-11-14 19:12:17 -0600

    Modificaciones

--- Sistema/formularios/productos.php
+++ Sistema/formularios/productos.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Este es el formulario del cliente</title>
-		<meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <link href="../css/bootstrap.css" rel="stylesheet" />
-	    <link href="../css/font-awesome/css/font-awesome.css" rel="stylesheet" />
-	    <link href="../css/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
-	   	<link href="../css/css-add/style.css" rel="stylesheet" />
-	    <link href="../css/css-add/main-style.css" rel="stylesheet" />
+    <head>
+        <title>Productos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/bootstrap.css" rel="stylesheet" />
+        <link href="../css/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        <link href="../css/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
+        <link href="../css/css-add/style.css" rel="stylesheet" />
+        <link href="../css/css-add/main-style.css" rel="stylesheet" />
         <style>
           .carousel-inner > .item > img,
           .carousel-inner > .item > a > img {
@@ -16,10 +16,10 @@
               margin: auto;
           }
         </style>
-	</head>
-	<body>
+    </head>
+    <body>
 
-		<div id="wrapper">
+        <div id="wrapper">
         <!-- navbar top -->
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar">
             <!-- navbar-header -->
@@ -164,10 +164,10 @@
                         <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Información<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="Sucursales.php">Sucursales</a>
+                                <a href="flot.html">Sucursales</a>
                             </li>
                             <li>
-                                <a href="Descuentos.php">Descuentos de temporada</a>
+                                <a href="morris.html">Descuentos de temporada</a>
                             </li>
                             <li>
                                 <a href="productos.php">Productos disponibles</a>
@@ -195,7 +195,7 @@
             <div class="row">
                 <!-- Page Header -->
                 <div class="col-lg-12">
-                    <h1 class="page-header">Tablero</h1>
+                    <h1 class="page-header">Productos</h1>
                 </div>
                 <!--End Page Header -->
             </div>
@@ -213,33 +213,6 @@
 
 
             <div class="row">
-                <!--quick info section -->
-                <div class="col-lg-3">
-                    <div class="alert alert-danger text-center">
-                        <i class="fa fa-calendar fa-3x"></i>&nbsp;<b>20 </b>Meetings Sheduled This Month
-
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="alert alert-success text-center">
-                        <i class="fa  fa-beer fa-3x"></i>&nbsp;<b>27 % </b>Profit Recorded in This Month  
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="alert alert-info text-center">
-                        <i class="fa fa-rss fa-3x"></i>&nbsp;<b>1,900</b> New Subscribers This Year
-
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="alert alert-warning text-center">
-                        <i class="fa  fa-pencil fa-3x"></i>&nbsp;<b>2,000 $ </b>Payment Dues For Rejected Items
-                    </div>
-                </div>
-                <!--end quick info section -->
-            </div>
-
-            <div class="row">
                 <div class="col-lg-8">
                     <!--Area chart example -->
                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -255,7 +228,7 @@
                         <div class="carousel-inner" role="listbox">
 
                           <div class="item active">
-                            <img src="../img/1.png" alt="Chania" width="460" height="345">
+                            <img src="../img/medicamento1.jpg" alt="Chania" width="460" height="345">
                             <div class="carousel-caption">
                               <h3>Visitenos</h3>
                               <p>Contamos con personal capacitado.</p>
@@ -263,7 +236,7 @@
                           </div>
 
                           <div class="item">
-                            <img src="../img/2.png" alt="Chania" width="460" height="345">
+                            <img src="../img/medicamento2.png" alt="Chania" width="460" height="345">
                             <div class="carousel-caption">
                               <h3>Estamos para servirle</h3>
                               <p>Su salud es nuestra preocupación.</p>
@@ -271,7 +244,7 @@
                           </div>
                         
                           <div class="item">
-                            <img src="../img/3.png" alt="Flower" width="460" height="345">
+                            <img src="../img/medicamento3.jpg" alt="Flower" width="460" height="345">
                             <div class="carousel-caption">
                               <h3>Cuente con nosotros</h3>
                               <p>Le atendemos con mucho gusto y aclaramos sus dudas.</p>
@@ -279,7 +252,7 @@
                           </div>
 
                           <div class="item">
-                            <img src="../img/4.png" alt="Flower" width="460" height="345">
+                            <img src="../img/medicamento4.jpg" alt="Flower" width="460" height="345">
                             <div class="carousel-caption">
                               <h3>Atención para usted</h3>
                               <p>No se conforme, busque lo mejor para su salud, busquenos.</p>
@@ -307,77 +280,82 @@
 
                 </div>
 
-                <div class="col-lg-4">
-                    <div class="panel panel-primary text-center no-boder">
-                        <div class="panel-body yellow">
-                            <i class="fa fa-bar-chart-o fa-3x"></i>
-                            <h3>20,741 </h3>
-                        </div>
-                        <div class="panel-footer">
-                            <span class="panel-eyecandy-title">Daily User Visits
-                            </span>
-                        </div>
-                    </div>
-                    <div class="panel panel-primary text-center no-boder">
-                        <div class="panel-body blue">
-                            <i class="fa fa-pencil-square-o fa-3x"></i>
-                            <h3>2,060 </h3>
-                        </div>
-                        <div class="panel-footer">
-                            <span class="panel-eyecandy-title">Pending Orders Found
-                            </span>
-                        </div>
-                    </div>
-                    <div class="panel panel-primary text-center no-boder">
-                        <div class="panel-body green">
-                            <i class="fa fa fa-floppy-o fa-3x"></i>
-                            <h3>20 GB</h3>
-                        </div>
-                        <div class="panel-footer">
-                            <span class="panel-eyecandy-title">New Data Uploaded
-                            </span>
-                        </div>
-                    </div>
-                    <div class="panel panel-primary text-center no-boder">
-                        <div class="panel-body red">
-                            <i class="fa fa-thumbs-up fa-3x"></i>
-                            <h3>2,700 </h3>
-                        </div>
-                        <div class="panel-footer">
-                            <span class="panel-eyecandy-title">New User Registered
-                            </span>
-                        </div>
-                    </div>
-                </div>
+            </div>   
 
-            </div>
-
-            <div class="row">
-                <div class="col-lg-4">
+            <h1><br>Productos</h1>
+            
+             <div class="row">
+                <div  class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
                     <div class="well">
-                        <!-- Notifications-->
-                        
-                        <!--End Notifications-->
+                        <div class="bg-info">
+                            <img src="../img/medicamento1.jpg" class="img-responsive">
+                            <b>NEUROBION DC 25000 HYPAK CON 1 JERINGA</b><br>
+                            <span class="label label-primary">3</span>
+                            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <br>
+                            <p>VITAMINA B1 (TIAMINA)   100 MG
+                            VITAMINA B6 (PIRIDOXINA)    100 MG
+                            VITAMINA B12 (CIANOCOBALAMINA, HIDROXOCOBALAMINA)   25000 MCG<p><br>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
+                <div  class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
                     <div class="well">
-                        <!-- Donut Example-->
-
-                        <!--End Donut Example-->
+                        <div class="bg-info">
+                            <img src="../img/medicamento2.png" class="img-responsive">
+                            <b>NEUROBION DC 25000 HYPAK CON 1 JERINGA</b><br>
+                            <span class="label label-primary">3</span>
+                            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <br>
+                            <p>VITAMINA B1 (TIAMINA)   100 MG
+                            VITAMINA B6 (PIRIDOXINA)    100 MG
+                            VITAMINA B12 (CIANOCOBALAMINA, HIDROXOCOBALAMINA)   25000 MCG<p><br>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
                     <div class="well">
-                        <!-- Chat Panel Example-->
-                        
-                        <!--End Chat Panel Example-->
+                        <div class="bg-info">
+                            <img src="../img/medicamento3.jpg" class="img-responsive">
+                            <b>NEUROBION DC 25000 HYPAK CON 1 JERINGA</b><br>
+                            <span class="label label-primary">3</span>
+                            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <br>
+                            VITAMINA B1 (TIAMINA)   100 MG
+                            VITAMINA B6 (PIRIDOXINA)    100 MG
+                            VITAMINA B12 (CIANOCOBALAMINA, HIDROXOCOBALAMINA)   25000 MCG<br><br><br>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-
-         
+                <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+                    <div class="well">
+                        <div class="bg-info">
+                            <img src="../img/medicamento4.jpg" class="img-responsive">
+                            <b>DOLO-NEUROBION DC AMPOLLAS</b><br>
+                            <span class="label label-primary">3</span>
+                            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <br>
+                            VITAMINA B1 (TIAMINA)   100 MG
+                            VITAMINA B6 (PIRIDOXINA)    100 MG
+                            VITAMINA B12 (CIANOCOBALAMINA, HIDROXOCOBALAMINA)   5 MG
+                            DICLOFENACO SODICO  75 MG
+                            LIDOCAINA   20 MG<br>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-4 col-lg-">
+                    <div class="well">
+                        <div class="bg-info">
+                            <img src="../img/medicamento5.jpg" class="img-responsive">
+                            <b>MEDICASP SHAMPOO X 130 ML</b><br>
+                            <span class="label label-primary">3</span>
+                            <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                            <br>
+                            SHAMPOO USO CAPILAR. USE DIARIAMENTE DURANTE 3 SEMANAS PARA ELIMINAR EL HONGO DEL CUERO CABELLUDO. LUEGO USAR 2 VECES POR SEMANA, PARA QUE LA CASPA NO REGRESE. O ACRITERIO DEL MEDICO.<br>
+                        </div>
+                    </div>
+                </div>
+            </div>    
 
 
         </div>
@@ -385,34 +363,17 @@
 
     </div>
     <!-- end wrapper -->
-    <br>
-    <div class="row">
-        <div class="well">
-            <h1><br>Productos</h1>
-            <div  class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-                <div class="bg-info">
-                    <img src="../img/m1.jpg" class="img-responsive">
-                    <b>NEUROBION DC 25000 HYPAK CON 1 JERINGA</b><br>
-                    <span class="label label-primary">3</span>
-                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                    <br>
-                    VITAMINA B1 (TIAMINA)   100 MG
-                    VITAMINA B6 (PIRIDOXINA)    100 MG
-                    VITAMINA B12 (CIANOCOBALAMINA, HIDROXOCOBALAMINA)   25000 MCG<br>
-                </div>
-            </div>
-        </div>
-    </div><br><br><br><br><br>
+    <br><br><br><br><br>
 
-		<script src="../css/plugins/jquery-1.10.2.js"></script>
-	    <script src="../css/plugins/bootstrap/bootstrap.min.js"></script>
-	    <script src="../css/plugins/metisMenu/jquery.metisMenu.js"></script>
-	    <!--aGREGADOS-->
-	    <script src="../css/plugins/pace/pace.js"></script>
-	    <script src="../js/scripts/siminta.js"></script>
-	    <!-- Page-Level Plugin Scripts-->
-	    <script src="../css/plugins/morris/raphael-2.1.0.min.js"></script>
-	    <script src="../css/plugins/morris/morris.js"></script>
-	    <script src="../js/scripts/dashboard-demo.js"></script>
-	</body>
+        <script src="../css/plugins/jquery-1.10.2.js"></script>
+        <script src="../css/plugins/bootstrap/bootstrap.min.js"></script>
+        <script src="../css/plugins/metisMenu/jquery.metisMenu.js"></script>
+        <!--aGREGADOS-->
+        <script src="../css/plugins/pace/pace.js"></script>
+        <script src="../js/scripts/siminta.js"></script>
+        <!-- Page-Level Plugin Scripts-->
+        <script src="../css/plugins/morris/raphael-2.1.0.min.js"></script>
+        <script src="../css/plugins/morris/morris.js"></script>
+        <script src="../js/scripts/dashboard-demo.js"></script>
+    </body>
 </html>
